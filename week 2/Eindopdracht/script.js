91885a8bd44272a1a181a47214326e79d1fdf40c
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2022-10-13 15:24:16 +0200

    klaar

--- week 2/Eindopdracht/script.js
+++ week 2/Eindopdracht/script.js
@@ -1,15 +1,17 @@
-$('.stack').click(function() {
-    $(".whiteCard").each(function(e) {
-      setTimeout(function() {
-        $(".whiteCard").eq(e).attr("class", "whiteCard");
-      }, e * 150)
+$('.stack').click(function () {
+    $(".whiteCard").each(function (e) {
+        setTimeout(function () {
+            $(".whiteCard").eq(e).attr("class", "whiteCard");
+        }, e * 150)
     });
-  });
-  
-  $('.spread').click(function() {
-    $(".whiteCard").each(function(e) {
-      setTimeout(function() {
-        $(".whiteCard").eq(e).attr("class", "whiteCard ani" + e);
-      }, e * 150)
+});
+
+$('.spread').click(function () {
+    $(".whiteCard").each(function (e) {
+        setTimeout(function () {
+            $(".whiteCard").eq(e).attr("class", "whiteCard ani" + e);
+        }, e * 150)
     });
-  });
+});
+
+
